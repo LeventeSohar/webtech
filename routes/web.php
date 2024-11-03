3bc1e35341e0211e78157c0af975e5b209e68caa
Dudu Dudu <dudutarsi@gmail.com>
2024-11-03 19:16:51 +0100

    CRUD setup migrations + migrations, model and controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ItemController;
 
 // Homepage route
 Route::get('/', [PageController::class, 'home']);
@@ -11,3 +12,9 @@ Route::get('/about', [PageController::class, 'about']);
 
 // Route for handling the form submission
 Route::post('/submit', [PageController::class, 'submitForm'])->name('submitForm');
+
+// Resourceful routes for items
+Route::resource('items', ItemController::class);
+
+// Route to display the items view
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
