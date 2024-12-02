ceb8e896ee9dcfcb368f6e72291c3f3ef0642354
dudu-sudo <dudutarsi@gmail.com>
2024-12-02 20:39:50 +0100

    coooking

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AnimalController;
+
+
+Route::resource('animals', AnimalController::class);
+
+
+//Static routes for now (except Adoption)
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/adopt', [AnimalController::class, 'adopt'])->name('adopt.index');
+
+Route::get('/guidelines', function () {
+    return 'Guidelines (to be implemented)';
+});
+
+Route::get('/donate', function () {
+    return 'Donate page (to be implemented)';
+});
+
+Route::get('/volunteer', function () {
+    return 'Volunteer page (to be implemented)';
+});
+
+Route::get('/blog', function () {
+    return 'Blog page (to be implemented)';
+});
+
+Route::get('/signin', function () {
+    return 'Sign-in page (to be implemented)';
+});
