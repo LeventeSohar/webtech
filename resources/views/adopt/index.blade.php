3617d1faf65dfba4fbcd5e973101a3387310efd4
hannararw <hrummelh@gmail.com>
2024-12-15 21:58:02 +0100

    YESS, the javascript is finally in a seperated file under resources/js/adopt.js. The tricky thing is, that you need to connect the ressources to public for vite so that they show up in your assets. After i figured all that out, I just misplaced the @vite in the index.blade.php so it didnt work and i could not see it at all in the network when trying to debug the website. But now it is working!

--- resources/views/adopt/index.blade.php
+++ resources/views/adopt/index.blade.php
@@ -77,45 +77,6 @@
         @endforeach
     </div>
 
-    <!-- JavaScript for functionality -->
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            
-            // 1. Checkbox functionalities
-            const newestCheckbox = document.getElementById('newest');
-            const oldestCheckbox = document.getElementById('oldest');
-            
-            // 2. Only one checkbox can be checked at once
-            newestCheckbox.addEventListener('change', function () {
-                if (this.checked) {
-                    oldestCheckbox.checked = false; 
-                }
-            });
-
-            oldestCheckbox.addEventListener('change', function () {
-                if (this.checked) {
-                    newestCheckbox.checked = false;
-                }
-            });
-
-            // "Read More" functionality
-            document.querySelectorAll('.read-more').forEach(function (button) {
-                button.addEventListener('click', function () {
-                    const animalId = this.getAttribute('data-id');
-                    const details = document.getElementById('details-' + animalId);
-                    const card = document.getElementById('animal-' + animalId);
-
-                    // Toggle visibility
-                    if (details.style.display === 'none') {
-                        details.style.display = 'block';
-                        card.style.flexDirection = 'column';
-                        this.textContent = 'Show less...';
-                    } else {
-                        details.style.display = 'none';
-                        this.textContent = 'Read more...';
-                    }
-                });
-            });
-        });
-    </script>
+    @vite(['resources/js/adopt.js'])
+    
 @endsection
